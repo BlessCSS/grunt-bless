8cf9e7f369323d1bb6d0693c7fe172ec88068ccb
Robert Haritonov <r@rhr.me>
2014-09-04 14:57:30 +0300

    Adding logging on file modification and creation.

--- tasks/bless.js
+++ tasks/bless.js
@@ -8,76 +8,89 @@
 'use strict';
 
 module.exports = function(grunt) {
-	var path = require('path'),
-		bless = require('bless'),
-		OVERWRITE_ERROR = 'The destination is the same as the source for file ',
-		OVERWRITE_EXCEPTION = 'Cowardly refusing to overwrite the source file.';
+    var chalk = require('chalk');
+    var path = require('path'),
+        bless = require('bless'),
+        OVERWRITE_ERROR = 'The destination is the same as the source for file ',
+        OVERWRITE_EXCEPTION = 'Cowardly refusing to overwrite the source file.';
 
-	grunt.registerMultiTask('bless', 'Split CSS files suitable for IE', function() {
+    grunt.registerMultiTask('bless', 'Split CSS files suitable for IE', function() {
 
-		var options = this.options({
-			cacheBuster: true,
-			cleanup: true,
-			compress: false,
-			force: grunt.option('force') || false,
-			imports: true
-		});
-		grunt.log.writeflags(options, 'options');
+        var options = this.options({
+            cacheBuster: true,
+            cleanup: true,
+            compress: false,
+            force: grunt.option('force') || false,
+            imports: true
+        });
+        grunt.log.writeflags(options, 'options');
 
-		grunt.util.async.forEach(this.files, function (input_files, next) {
-			var data = '';
+        grunt.util.async.forEach(this.files, function (input_files, next) {
+            var data = '';
 
-			// If we are not forcing the build refuse to overwrite the
-			// source file.
-			if (!options.force && input_files.src.indexOf(input_files.dest) >= 0) {
-				grunt.log.error(OVERWRITE_ERROR + input_files.dest);
-				throw grunt.util.error(OVERWRITE_EXCEPTION);
-			}
+            // If we are not forcing the build refuse to overwrite the
+            // source file.
+            if (!options.force && input_files.src.indexOf(input_files.dest) >= 0) {
+                grunt.log.error(OVERWRITE_ERROR + input_files.dest);
+                throw grunt.util.error(OVERWRITE_EXCEPTION);
+            }
 
-			// read and concat files
-			input_files.src.forEach(function (file) {
-				data += grunt.file.read(file);
-			});
+            // read and concat files
+            input_files.src.forEach(function (file) {
+                data += grunt.file.read(file);
+            });
 
 
-			new (bless.Parser)({
-				output: input_files.dest,
-				options: options
-			}).parse(data, function (err, files, numSelectors) {
-				if (err) {
-					grunt.log.error(err);
-					throw grunt.util.error(err);
-				}
+            new (bless.Parser)({
+                output: input_files.dest,
+                options: options
+            }).parse(data, function (err, files, numSelectors) {
+                if (err) {
+                    grunt.log.error(err);
+                    throw grunt.util.error(err);
+                }
 
-				// print log message
-				var msg = 'Found ' + numSelectors + ' selector';
-				if (numSelectors !== 1) {
-					msg += 's';
-				}
-				msg += ', ';
-				if (files.length > 1) {
-					msg += 'splitting into ' + files.length + ' files.';
-				} else {
-					msg += 'not splitting.';
-				}
-				grunt.log.verbose.writeln(msg);
+                // print log message
+                var msg = 'Found ' + numSelectors + ' selector';
+                if (numSelectors !== 1) {
+                    msg += 's';
+                }
+                msg += ', ';
+                if (files.length > 1) {
+                    msg += 'splitting into ' + files.length + ' files.';
+                } else {
+                    msg += 'not splitting.';
+                }
+                grunt.log.verbose.writeln(msg);
 
-				// write processed file(s)
-				files.forEach(function (file) {
+                // write processed file(s)
+                var filesLength = files.length;
+                var logModified = (filesLength > 1);
+                var writeCount = 0;
 
-					// Because files is an array there is no way of finding the
-					// first file to add the banner without looping through them.
-					// 
-					// Since we are already doing that...
+                files.forEach(function (file) {
 
-					if (options.banner && file.filename === input_files.dest) {
-						file.content = options.banner + grunt.util.linefeed + file.content;
-					}
+                    // Because files is an array there is no way of finding the
+                    // first file to add the banner without looping through them.
+                    //
+                    // Since we are already doing that...
 
-					grunt.file.write(file.filename, file.content);
-				});
-			});
-			next();
-		});
-	});
+                    if (options.banner && file.filename === input_files.dest) {
+                        file.content = options.banner + grunt.util.linefeed + file.content;
+                    }
+
+                    grunt.file.write(file.filename, file.content);
+
+                    writeCount++;
+
+                    if (logModified) {
+                        var lastSentence = filesLength === writeCount ? 'modified' : 'created';
+
+                        grunt.log.writeln('File ' + chalk.cyan(file.filename) + ' ' + lastSentence + '.');
+                    }
+                });
+            });
+            next();
+        });
+    });
 };
