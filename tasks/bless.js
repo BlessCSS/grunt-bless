c42690014039365a815427aae76b9648ff8530df
Luke Ring <luke.ring@vu.edu.au>
2015-03-23 15:26:31 +1100

    change to use bless 4.0.0 based upon https://github.com/2gis/gulp.bless/blob/master/index.js

--- tasks/bless.js
+++ tasks/bless.js
@@ -9,7 +9,7 @@
 
 module.exports = function(grunt) {
 	var path = require('path'),
-		bless = require('bless'),
+		bless = require('bless4'),
 		util = require('util'),
 		chalk = require('chalk'),
 		OVERWRITE_ERROR = 'The destination is the same as the source for file ',
@@ -56,7 +56,94 @@ module.exports = function(grunt) {
 				data += grunt.file.read(inputFile);
 			}
 
+			var result = bless(data, 4079);
 
+			//make sure the parsing worked
+			if (!result.data.length > 1) {
+				grunt.log.error('Blessing failed');
+				throw grunt.util.error('Blessing Failed');
+			}
+
+			if (options.logCount) {
+				var overLimit = result.numSelectors > limit;
+				var _numSelectors = chalk.green(result.numSelectors);
+
+				if (overLimit) {
+					_numSelectors = chalk.red(result.numSelectors);
+				} else if (numSelectors > options.warnLimit ) {
+					_numSelectors = chalk.yellow(result.numSelectors);
+				}
+
+				var coungMsg = path.basename(outPutfileName) + ' has ' + _numSelectors + ' CSS selectors.';
+
+				if (overLimit) {
+					grunt.log.errorlns(coungMsg + ' IE8-9 will read only first ' + limit + '!');
+				} else if (options.logCount !== 'warn') {
+					grunt.log.oklns(coungMsg);
+				}
+			}
+
+			// print log message
+			var msg = 'Found ' + result.numSelectors + ' selector';
+			if (result.numSelectors !== 1) {
+				msg += 's';
+			}
+			msg += ', ';
+			if (result.data.length > 1) {
+				msg += 'splitting into ' + result.data.length + ' files.';
+			} else {
+				msg += 'not splitting.';
+			}
+			grunt.log.verbose.writeln(msg);
+
+			var names = [outPutfileName];
+
+			names = names.concat(result.data.map(function(conents, i) {
+                var ext = path.extname(outPutfileName);
+                var basename = path.basename(outPutfileName, ext);
+                var dirname = path.dirname(outPutfileName);
+
+                return path.join(dirname, basename + '-blessed' + i + ext);
+            }));
+
+            var imports = names.slice(1).map(function(name) {
+                var basename = path.basename(name);
+                return '@import "' + basename + '";';
+            });
+            result.data.unshift(imports.join('\n') + '\n');
+
+            // write processed file(s)
+			var filesLength = result.data.length;
+			var logModified = (filesLength > 1);
+			var writeCount = 0;
+
+			if (writeFiles) {
+				result.data.forEach(function (contents, i) {
+
+					// Because files is an array there is no way of finding the
+					// first file to add the banner without looping through them.
+					//
+					// Since we are already doing that...
+
+					// if (options.banner && file.filename === file.dest) {
+					// 	file.content = options.banner + grunt.util.linefeed + file.content;
+					// }
+
+					grunt.file.write(names[i], contents);
+
+					writeCount++;
+
+					if (logModified) {
+						var lastSentence = filesLength === writeCount ? 'modified' : 'created';
+
+						grunt.log.writeln('File ' + chalk.cyan(names[i]) + ' ' + lastSentence + '.');
+					}
+				});
+			}
+
+			next();
+			return;
+			/////////////////////
 			new (bless.Parser)({
 				output: outPutfileName,
 				options: options
