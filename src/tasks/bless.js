7e4353037e73b2dc9bcac7115735d8868e624d3d
Aaron Spaulding <aaron@sachimp.com>
2016-03-07 13:33:13 -0500

    Fix for Issue #37

--- src/tasks/bless.js
+++ src/tasks/bless.js
@@ -124,14 +124,16 @@ module.exports = function(grunt) {
 					header += options.banner + grunt.util.linefeed;
 				}
 
-				header += file_utils.imports({
-					numFiles: filesLength,
-					output: outPutfileName,
-					suffix: suffix,
-					linefeed: (options.compress ? '' : grunt.util.linefeed)
-				});
+				if (options.imports) {
+					header += file_utils.imports({
+						numFiles: filesLength,
+						output: outPutfileName,
+						suffix: suffix,
+						linefeed: (options.compress ? '' : grunt.util.linefeed)
+					});
 
-				header += grunt.util.linefeed;
+					header += grunt.util.linefeed;
+				}
 
 				// The main file is always the last one in the array.
 				// So add the header here before we iterate.
